168a519061734d97bd4075ec58e2a29922bed23e
AlexanderGoryachev <Goryachev.Alexander.S@gmail.com>
2013-09-13 16:54:45 +0400

    Initial commit, 16:54 Friday 13

---
+++ PhoneApp2/MainPage.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using PhoneApp2.Resources;
+
+using UpdateTileAgent;
+using Microsoft.Phone.Scheduler;
+using System.IO.IsolatedStorage;
+using System.IO;
+
+namespace PhoneApp2
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        const string UpdateTileAgentName = "Agent-Tile";
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            this.Loaded += new RoutedEventHandler(MainPage_Loaded);
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            PeriodicTask myPeriodicTask = ScheduledActionService.Find(UpdateTileAgentName) as PeriodicTask;
+
+            if (myPeriodicTask != null)
+            {
+                try
+                {
+                    ScheduledActionService.Remove(UpdateTileAgentName);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Невозможно удалить ранее созданный сервис:" + ex.Message);
+                }
+            }
+
+            myPeriodicTask = new PeriodicTask(UpdateTileAgentName);
+            myPeriodicTask.Description = "Agent-Tile";
+
+            try
+            {
+                ScheduledActionService.Add(myPeriodicTask);
+
+#if DEBUG
+                ScheduledActionService.LaunchForTest(UpdateTileAgentName, TimeSpan.FromSeconds(10));
+#endif
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Невозможно создать сервис:" + ex.Message);
+            }
+        }
+
+        private async void LockHelper(string filePathOfTheImage, bool isAppResource)
+        {
+            try
+            {
+                var isProvider = Windows.Phone.System.UserProfile.LockScreenManager.IsProvidedByCurrentApplication;
+                if (!isProvider)
+                {
+                    // If you're not the provider, this call will prompt the user for permission.
+                    // Calling RequestAccessAsync from a background agent is not allowed.
+                    var op = await Windows.Phone.System.UserProfile.LockScreenManager.RequestAccessAsync();
+
+                    // Only do further work if the access was granted.
+                    isProvider = op == Windows.Phone.System.UserProfile.LockScreenRequestResult.Granted;
+                }
+
+                if (isProvider)
+                {
+                    // At this stage, the app is the active lock screen background provider.
+
+                    // The following code example shows the new URI schema.
+                    // ms-appdata points to the root of the local app data folder.
+                    // ms-appx points to the Local app install folder, to reference resources bundled in the XAP package.
+                    var schema = isAppResource ? "ms-appx:///" : "ms-appdata:///Local/";
+                    var uri = new Uri(schema + filePathOfTheImage, UriKind.Absolute);
+
+                    // Set the lock screen background image.
+                    Windows.Phone.System.UserProfile.LockScreen.SetImageUri(uri);
+
+                    // Get the URI of the lock screen background image.
+                    var currentImage = Windows.Phone.System.UserProfile.LockScreen.GetImageUri();
+                    System.Diagnostics.Debug.WriteLine("The new lock screen background image is set to {0}", currentImage.ToString());
+                }
+                else
+                {
+                    MessageBox.Show("You said no, so I can't update your background.");
+                }
+            }
+            catch (System.Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.ToString());
+            }
+        }
+
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
